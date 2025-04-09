f719ce6238ca3f6a11fd524bef4956ba8f038bbb
mazzamassy <massi.mazza.2006@gmail.com>
2025-04-09 15:27:56 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -321,5 +321,6 @@ app.use(async (ctx, next) => {
 
 bot.catch((err) => console.error("Bot error:", err));
 app.use(oakCors());
+bot.start(); 
 import { serve } from "https://deno.land/std@0.223.0/http/server.ts";
 serve(app.handle);
